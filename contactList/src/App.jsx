914b4223060fd6259857a90c06565cce47de80c7
Cassandra Rockwell <cassielynn99@icloud.com>
2023-09-07 11:37:22 -0400

    initial commit.

---
+++ contactList/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+// to view: http://localhost:5173/
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
